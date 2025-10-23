827dca7e22af048eaacdad64faa470c6dafa9fb6
PinkiePie <kir.it.prog@gmail.com>
2025-10-23 23:06:51 +0700

    Initial commit: создание WPF проекта с разделением на UI и Domain

---
+++ Library/UI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Catalog_Click(object sender, RoutedEventArgs e)
+        {
+            BookWindow bookWindow = new BookWindow();
+            bookWindow.Show();
+        }
+
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IssueBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReturnBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
