34e567f5cc16b0d752fb9920665c917b4b247a5b
PinkiePie <kir.it.prog@gmail.com>
2025-10-26 23:09:21 +0700

    Initial commit: создание WPF проекта с разделением на UI и Domain

---
+++ Library/UI/BookWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UI
+{
+    public partial class BookWindow : Window
+    {
+        public BookWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearFilters_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Genre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Books_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
